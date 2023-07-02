04143ac5690ab305094904dda06de3ae9104b4db
moh7386 <104713409+moh7386@users.noreply.github.com>
2023-07-02 17:44:50 +0300

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -13,7 +13,7 @@ conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, 
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(invocar|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
 export default handler
