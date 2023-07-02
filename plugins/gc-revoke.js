625cb9d8dd039bff07486c88dcf96326f7453153
moh7386 <104713409+moh7386@users.noreply.github.com>
2023-07-03 02:12:31 +0300

    Update gc-revoke.js

--- plugins/gc-revoke.js
+++ plugins/gc-revoke.js
@@ -3,7 +3,7 @@
 let handler = async(m, { conn }) => {
 let revoke = await conn.groupRevokeInvite(m.chat)
 await conn.reply(m.chat, `🔹️ *_Se restableció con éxito el link del grupo._*\n♾ • Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m)}
-handler.command = ['resetlink', 'revoke']
+handler.command = ['ريست', 'revoke']
 handler.botAdmin = true
 handler.admin = true
 handler.group = true
