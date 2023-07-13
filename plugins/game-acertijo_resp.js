a567a8fde4dc0975e38f78212e5e418f10d53283
moh7386 <104713409+moh7386@users.noreply.github.com>
2023-07-13 08:58:23 +0300

    Update game-acertijo_resp.js

--- plugins/game-acertijo_resp.js
+++ plugins/game-acertijo_resp.js
@@ -1,13 +1,24 @@
 import similarity from 'similarity'
 
-
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import fs from 'fs'
+ 
 
 const threshold = 0.72
 
 let handler = m => m
 
 handler.before = async function (m) {
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let f=`❜🤹🏻‍♀️ *أنتهت* 🤹🏻‍♂️❛
+*✦↫👤المجاوب :*「${taguser}」
+*✦↫💸الجائزة :*「5k」
+*✦↫⌚الوقت :*「30 ثانية」
+*•ه─────────────ه•*
+*❍.إدارة : 𓆸*
+*「𝑹𝑨𝑪𝑯𝑬𝑳 🪶𝑩𝑶𝑻 」*`
 
+   
     let id = m.chat
 
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0
@@ -26,7 +37,7 @@ handler.before = async function (m) {
 
             global.db.data.users[m.sender].exp += this.tekateki[id][2]
 
-            m.reply(`*أحسنت إجابة صحيحة!*\n *كسبت*: نقطة تابع اللعب`)
+            m.reply(f, null, { mentions: conn.parseMention(f) })
 
             clearTimeout(this.tekateki[id][3])
 
