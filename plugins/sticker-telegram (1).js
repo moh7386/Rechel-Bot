75d4b80c709969039ebd8e2a8b422f71050ff545
moh7386 <104713409+moh7386@users.noreply.github.com>
2023-07-23 22:01:23 +0300

    Add files via upload

---
+++ plugins/sticker-telegram (1).js
@@ -0,0 +1,30 @@
+import fetch from "node-fetch"
+import { sticker } from '../lib/sticker.js'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙀𝙉𝙇𝘼𝘾𝙀 𝘿𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal\n\n𝙀𝙉𝙏𝙀𝙍 𝙏𝙃𝙀 𝙎𝙏𝙄𝘾𝙆𝙀𝙍 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈 𝙇𝙄𝙉𝙆\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀:\n${usedPrefix + command} https://t.me/addstickers/Porcientoreal`
+    if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `╰⊱❗️⊱ *𝙇𝙊 𝙐𝙎𝙊́ 𝙈𝘼𝙇 | 𝙐𝙎𝙀𝘿 𝙄𝙏 𝙒𝙍𝙊𝙉𝙂* ⊱❗️⊱╮\n\n𝙇𝘼 𝙐𝙍𝙇 𝙀𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏𝘼\n𝙏𝙃𝙀 𝙐𝙍𝙇 𝙄𝙎 𝙄𝙉𝘾𝙊𝙍𝙍𝙀𝘾𝙏`
+    let packName = args[0].replace("https://t.me/addstickers/", "") 
+    let gas = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: "GET", headers: { "User-Agent": "GoogleBot" } })
+    if (!gas.ok) throw eror
+    let json = await gas.json()
+    m.reply(`*عدد الملصقات:* ${json.result.stickers.length}
+*الوقت المتوقع:* ${json.result.stickers.length * 1.5} ثانية`.trim())
+    for (let i = 0; i < json.result.stickers.length; i++) {
+        let fileId = json.result.stickers[i].thumb.file_id
+        let gasIn = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
+        let jisin = await gasIn.json()
+        let stiker = await sticker(false, "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + jisin.result.file_path, global.packname, global.author)
+        await delay(5000)
+        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: 'احبكم', body: `h`, mediaType: 2, sourceUrl: 'انا'}}}, { quoted: m })
+        await delay(3000)
+    }
+    throw `*𝙇𝙄𝙎𝙏𝙊𝙊𝙊𝙊𝙊 ✅️*`
+}
+handler.help = ['stikertele *<url>*']
+handler.tags = ['sticker', 'downloader']
+handler.command = /^(stic?kertele(gram)?)$/i
+
+
+export default handler
+
+const delay = time => new Promise(res => setTimeout(res, time))
